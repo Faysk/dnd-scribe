a07a3f604e122c5c710b3e3d2a6a71b0b758452f
Faysk <faysk.nan@gmail.com>
2026-09-04 18:48:17 +0100

    test(web-next): fix vitest import resolution

--- apps/web/tests/unit/class-names.test.ts
+++ apps/web/tests/unit/class-names.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from 'vitest'
 
-import { cn } from '@/lib/class-names'
+import { cn } from '../../lib/class-names'
 
 describe('cn', () => {
   it('joins only truthy class values in order', () => {
